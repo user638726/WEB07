730455a4aca15f3287bdff95fcc040003c922f7a
user638726 <lienjackson35@gmail.com>
2025-01-13 15:27:16 +0800

    增加知道電影ID

--- front/order.php
+++ front/order.php
@@ -25,49 +25,58 @@
     background: #999;
 }
 </style>
-<h3 class='ct'>線上訂票</h3>
-<form action="#">
-    <table class="order-form">
-        <tr>
-            <td>電影：</td>
-            <td><select name="movie" id="movie"></select></td>
-        </tr>
-        <tr>
-            <td>日期：</td>
-            <td><select name="date" id="date"></select></td>
-        </tr>
-        <tr>
-            <td>場次：</td>
-            <td><select name="session" id="session"></select></td>
-        </tr>
-        <tr>
-            <td colspan='2' class='ct'>
-                <input type="button" value="確定">
-                <input type="reset" value="重置">
-            </td>
-        </tr>
-    </table>
-</form>
+<div id="order">
+    <h3 class='ct'>線上訂票</h3>
+    <form action="#">
+        <table class="order-form">
+            <tr>
+                <td>電影：</td>
+                <td><select name="movie" id="movie"></select></td>
+            </tr>
+            <tr>
+                <td>日期：</td>
+                <td><select name="date" id="date"></select></td>
+            </tr>
+            <tr>
+                <td>場次：</td>
+                <td><select name="session" id="session"></select></td>
+            </tr>
+            <tr>
+                <td colspan='2' class='ct'>
+                    <input type="button" value="確定" onclick="booking()">
+                    <input type="reset" value="重置">
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
+<div id="booking" style="display:none">
+
+
+</div>
 
 <script>
 getMovies();
 let id = new URLSearchParams(location.href).get('id');
 //console.log(id);
+let movie = {};
 $("#movie").on("change", function() {
     getDays();
 })
+
 $("#date").on("change", function() {
     getSessions();
 })
 
 function getMovies() {
     $.get("api/get_movies.php", function(movies) {
-        console.log(movies);
+        //console.log(movies);
         $("#movie").html(movies);
 
         if (parseInt(id) > 0) {
             $(`#movie option[value='${id}']`).prop('selected', true);
         }
+
         getDays();
     })
 }
@@ -77,9 +86,8 @@ function getDays() {
         movie: $("#movie").val()
     }, function(days) {
         $("#date").html(days);
-        getSessions()
+        getSessions();
     })
-
 }
 
 function getSessions() {
@@ -89,6 +97,21 @@ function getSessions() {
     }, function(sessions) {
         $("#session").html(sessions);
     })
+
+}
+
+function booking() {
+    movie = {
+        id: $("#movie").val(),
+        name: $("#movie option:selected").text(),
+        date: $("#date").val(),
+        session: $("#session").val()
+    }
+
+    $("#booking").html(
+        `${movie.id},${movie.date},${movie.name},<button  onclick="$('#order,#booking').toggle()">上一步</button>`)
+
+    $("#booking,#order").toggle();
 
 }
 </script>
