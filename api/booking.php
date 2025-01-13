a36b26f0aeaed32c7478f80343d64a22a46da119
user638726 <lienjackson35@gmail.com>
2025-01-13 16:14:40 +0800

    增加座位圖

--- api/booking.php
+++ api/booking.php
@@ -7,15 +7,40 @@
 
 #movieInfo {
     width: 540px;
-    height: 100px;
+    height: 120px;
     margin: auto;
     background: #ccc;
     padding: 10px 100px;
 }
+
+#info {
+    background-image: url("icon/03D04.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    padding: 19px 110px 14px 110px;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.seat {
+    width: 64px;
+    height: 85px;
+    text-align: center;
+    padding: 2px;
+}
 </style>
 <?php  include_once "db.php";?>
 
-<div id="info"></div>
+<div id="info">
+    <?php 
+        for($i=0;$i<20;$i++){
+            echo "<div class='seat'>";
+            echo  floor($i/5)+1 ."排".($i%5+1)."號";
+            echo "</div>";
+        }
+    ?>
+
+</div>
 <div id="movieInfo">
     <div>您選擇的電影是：<?=$_GET['name'];?></div>
     <div>您選擇的時刻是：<?=$_GET['date']."&nbsp;&nbsp;".$_GET['session'];?></div>
